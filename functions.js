7e302c2796f9ed8b76fcf8402b2d7973e00b2d1e
JelkeJL <jelkejoosen@hotmail.com>
2018-04-25 13:37:42 +0200

    Current location map

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ $.getJSON("data.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 
 });
-
+/*
 function initMap() {
     var GH_london = {lat: 51.6633, lng: -0.0923};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -14,3 +14,61 @@ function initMap() {
         map: map
     });
 }
+*/
+
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.863002, lng: 4.678974},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow;
+        
+        //var image = 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/apple/129/woman-walking_1f6b6-200d-2640-fe0f.png'
+        //walk = new google.maps.Marker({
+          //icon: image
+        //});
+
+// Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+           infoWindow.setPosition(pos);
+           infoWindow.setContent('You are here.');
+           infoWindow.open(map);
+            map.setCenter(pos);
+
+          var image = {
+            url : 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/apple/129/woman-walking_1f6b6-200d-2640-fe0f.png',
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0), // anchor
+          }
+          var marker = new google.maps.Marker({
+            position: pos,
+            map:map,
+            icon: image,
+          });
+
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map,marker);
+      }
